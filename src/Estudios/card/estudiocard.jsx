573893cdb1a9d82d6d09349feb6815881632b7b4
Rubén <gutierrezruben657@gmail.com>
2024-08-13 11:48:03 +0200

    xd
    xd

--- src/Estudios/card/estudiocard.jsx
+++ src/Estudios/card/estudiocard.jsx
@@ -3,7 +3,7 @@
 function Estudio ({curso,children}) 
 {
 return(
-<div style={{display:"flex",flexDirection:"column", width:"300px"}}>
+<div style={{display:"flex",flexDirection:"column", width:"230px"}}>
 <p style={{fontSize:"30px"}}><b>{curso}</b></p> 
 <span>{children}</span>
 
